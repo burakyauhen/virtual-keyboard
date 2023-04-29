0a44a09810439b7f0b03c74a1ce2b923efef2804
Burak Yauhen <burakyauhen@gmail.com>
2023-04-29 12:50:12 +0300

    feat: add setting of textarea

--- src/keys.js
+++ src/keys.js
@@ -1,191 +1,194 @@
 const keys = [
   [{
-    eng: '`', rus: 'ё', engSup: '~', rusSup: 'Ё', structure: '<div class="key"></div>',
+    keyCode: 'Backquote', en: '`', enS: '~', enC: '`', ru: 'ё', ruS: 'Ё', ruC: 'Ё', structure: '<div class="key key-fun"></div>',
   },
   {
-    eng: '1', rus: '1', engSup: '!', rusSup: '!', structure: '<div class="key"></div>',
+    keyCode: 'Digit1', en: '1', enS: '!', enC: '`', ru: '1', ruS: '!', ruC: '1', structure: '<div class="key"></div>',
   },
   {
-    eng: '2', rus: '2', engSup: '@', rusSup: '"', structure: '<div class="key"></div>',
+    keyCode: 'Digit2', en: '2', enS: '@', enC: '2', ru: '2', ruS: '"', ruC: '2', structure: '<div class="key"></div>',
   },
   {
-    eng: '3', rus: '3', engSup: '#', rusSup: '№', structure: '<div class="key"></div>',
+    keyCode: 'Digit3', en: '3', enS: '#', enC: '3', ru: '3', ruS: '№', ruC: '3', structure: '<div class="key"></div>',
   },
   {
-    eng: '4', rus: '4', engSup: '$', rusSup: ';', structure: '<div class="key"></div>',
+    keyCode: 'Digit4', en: '4', enS: '$', enC: '4', ru: '4', ruS: ';', ruC: '4', structure: '<div class="key"></div>',
   },
   {
-    eng: '5', rus: '5', engSup: '%', rusSup: '%', structure: '<div class="key"></div>',
+    keyCode: 'Digit5', en: '5', enS: '%', enC: '5', ru: '%', ruS: '5', ruC: '5', structure: '<div class="key"></div>',
   },
   {
-    eng: '6', rus: '6', engSup: '^', rusSup: ':', structure: '<div class="key"></div>',
+    keyCode: 'Digit6', en: '6', enS: '^', enC: '`', ru: '6', ruS: ':', ruC: '6', structure: '<div class="key"></div>',
   },
   {
-    eng: '7', rus: '7', engSup: '&', rusSup: '?', structure: '<div class="key"></div>',
+    keyCode: 'Digit7', en: '7', enS: '&', enC: '7', ru: '7', ruS: '?', ruC: '7', structure: '<div class="key"></div>',
   },
   {
-    eng: '8', rus: '8', engSup: '*', rusSup: '*', structure: '<div class="key"></div>',
+    keyCode: 'Digit8', en: '8', enS: '*', enC: '8', ru: '8', ruS: '*', ruC: '8', structure: '<div class="key"></div>',
   },
   {
-    eng: '9', rus: '9', engSup: '(', rusSup: '(', structure: '<div class="key"></div>',
+    keyCode: 'Digit9', en: '9', enS: '(', enC: '9', ru: '9', ruS: '(', ruC: '9', structure: '<div class="key"></div>',
   },
   {
-    eng: '0', rus: '0', engSup: ')', rusSup: ')', structure: '<div class="key"></div>',
+    keyCode: 'Digit0', en: '0', enS: ')', enC: '0', ru: '0', ruS: ')', ruC: '0', structure: '<div class="key"></div>',
   },
   {
-    eng: '-', rus: '-', engSup: '_', rusSup: '_', structure: '<div class="key"></div>',
+    keyCode: 'Minus', en: '-', enS: '_', enC: '-', ru: '-', ruS: '_', ruC: '-', structure: '<div class="key"></div>',
   },
   {
-    eng: '=', rus: '=', engSup: '+', rusSup: '+', structure: '<div class="key"></div>',
+    keyCode: 'Equal', en: '=', enS: '+', enC: '=', ru: '=', ruS: '+', ruC: '=', structure: '<div class="key"></div>',
   },
   {
-    eng: 'Backspace', rus: 'Backspace', structure: '<div class="key"></div>',
+    keyCode: 'Backspace', en: 'Backspace', enS: 'Backspace', enC: 'Backspace', ru: 'Backspace', ruS: 'Backspace', ruC: 'Backspace', structure: '<div class="key key-fun backspace"></div>',
   }],
   [{
-    eng: 'Tab', rus: 'Tab', structure: '<div class="key"></div>',
+    keyCode: 'Tab', en: 'Tab', enS: 'Tab', enC: '`', ru: 'Tab', ruS: 'Tab', ruC: 'Tab', structure: '<div class="key key-fun"></div>',
   },
   {
-    eng: 'Q', rus: 'Й', structure: '<div class="key"></div>',
+    keyCode: 'KeyQ', en: 'q', enS: 'Q', enC: 'Q', ru: 'й', ruS: 'Й', ruC: 'Й', structure: '<div class="key"></div>',
   },
   {
-    eng: 'W', rus: 'Ц', structure: '<div class="key"></div>',
+    keyCode: 'KeyW', en: 'w', enS: 'W', enC: 'W', ru: 'ц', ruS: 'Ц', ruC: 'Ц', structure: '<div class="key"></div>',
   },
   {
-    eng: 'E', rus: 'У', structure: '<div class="key"></div>',
+    keyCode: 'KeyE', en: 'e', enS: 'E', enC: 'E', ru: 'у', ruS: 'У', ruC: 'У', structure: '<div class="key"></div>',
   },
   {
-    eng: 'R', rus: 'К', structure: '<div class="key"></div>',
+    keyCode: 'KeyR', en: 'r', enS: 'R', enC: 'R', ru: 'к', ruS: 'К', ruC: 'К', structure: '<div class="key"></div>',
   },
   {
-    eng: 'T', rus: 'Е', structure: '<div class="key"></div>',
+    keyCode: 'KeyT', en: 't', enS: 'T', enC: 'T', ru: 'е', ruS: 'Е', ruC: 'Е', structure: '<div class="key"></div>',
   },
   {
-    eng: 'Y', rus: 'Н', structure: '<div class="key"></div>',
+    keyCode: 'KeyY', en: 'y', enS: 'Y', enC: 'Y', ru: 'н', ruS: 'Н', ruC: 'Н', structure: '<div class="key"></div>',
   },
   {
-    eng: 'U', rus: 'Г', structure: '<div class="key"></div>',
+    keyCode: 'KeyU', en: 'u', enS: 'U', enC: 'U', ru: 'г', ruS: 'Г', ruC: 'Г', structure: '<div class="key"></div>',
   },
   {
-    eng: 'I', rus: 'Ш', structure: '<div class="key"></div>',
+    keyCode: 'KeyI', en: 'i', enS: 'I', enC: 'I', ru: 'ш', ruS: 'Ш', ruC: 'Ш', structure: '<div class="key"></div>',
   },
   {
-    eng: 'O', rus: 'Щ', structure: '<div class="key"></div>',
+    keyCode: 'KeyO', en: 'o', enS: 'O', enC: 'O', ru: 'щ', ruS: 'Щ', ruC: 'Щ', structure: '<div class="key"></div>',
   },
   {
-    eng: 'P', rus: 'З', structure: '<div class="key"></div>',
+    keyCode: 'KeyP', en: 'p', enS: 'P', enC: 'P', ru: 'з', ruS: 'З', ruC: 'З', structure: '<div class="key"></div>',
   },
   {
-    eng: '[', rus: 'Х', structure: '<div class="key"></div>',
+    keyCode: 'BracketLeft', en: '[', enS: '{', enC: '[', ru: 'х', ruS: 'Х', ruC: 'Х', structure: '<div class="key"></div>',
   },
   {
-    eng: ']', rus: 'Ъ', structure: '<div class="key"></div>',
+    keyCode: 'BracketRight', en: ']', enS: '}', enC: ']', ru: 'ъ', ruS: 'Ъ', ruC: 'Ъ', structure: '<div class="key"></div>',
   },
   {
-    eng: '\\', rus: '\\', engSup: '|', rusSup: '/', structure: '<div class="key"></div>',
+    keyCode: 'Backslash', en: '\\', enS: '|', enC: '\\', ru: '\\', ruS: '/', ruC: '\\', structure: '<div class="key"></div>',
+  },
+  {
+    keyCode: 'Delete', en: 'Del', enS: 'Del', enC: 'Del', ru: 'Del', ruS: 'Del', ruC: 'Del', structure: '<div class="key key-fun"></div>',
   }],
   [{
-    eng: 'Caps Lock', rus: 'Caps Lock', structure: '<div class="key key-fun capslock"></div>',
+    keyCode: 'CapsLock', en: 'Caps lock', enS: 'Caps lock', enC: 'Caps lock', ru: 'Caps lock', ruS: 'Caps lock', ruC: 'Caps lock', structure: '<div class="key key-fun capslock"></div>',
   },
   {
-    eng: 'A', rus: 'Ф', structure: '<div class="key"></div>',
+    keyCode: 'KeyA', en: 'a', enS: 'A', enC: 'A', ru: 'ф', ruS: 'Ф', ruC: 'Ф', structure: '<div class="key"></div>',
   },
   {
-    eng: 'S', rus: 'Ы', structure: '<div class="key"></div>',
+    keyCode: 'KeyS', en: 's', enS: 'S', enC: 'S', ru: 'ы', ruS: 'Ы', ruC: 'Ы', structure: '<div class="key"></div>',
   },
   {
-    eng: 'D', rus: 'В', structure: '<div class="key"></div>',
+    keyCode: 'KeyD', en: 'd', enS: 'D', enC: 'D', ru: 'в', ruS: 'В', ruC: 'В', structure: '<div class="key"></div>',
   },
   {
-    eng: 'F', rus: 'А', structure: '<div class="key"></div>',
+    keyCode: 'KeyF', en: 'f', enS: 'F', enC: 'F', ru: 'а', ruS: 'А', ruC: 'А', structure: '<div class="key"></div>',
   },
   {
-    eng: 'G', rus: 'П', structure: '<div class="key"></div>',
+    keyCode: 'KeyG', en: 'g', enS: 'G', enC: 'G', ru: 'п', ruS: 'П', ruC: 'П', structure: '<div class="key"></div>',
   },
   {
-    eng: 'H', rus: 'Р', structure: '<div class="key"></div>',
+    keyCode: 'KeyH', en: 'h', enS: 'H', enC: 'H', ru: 'р', ruS: 'Р', ruC: 'Р', structure: '<div class="key"></div>',
   },
   {
-    eng: 'J', rus: 'О', structure: '<div class="key"></div>',
+    keyCode: 'KeyJ', en: 'j', enS: 'J', enC: 'J', ru: 'о', ruS: 'О', ruC: 'О', structure: '<div class="key"></div>',
   },
   {
-    eng: 'K', rus: 'Л', structure: '<div class="key"></div>',
+    keyCode: 'KeyK', en: 'k', enS: 'K', enC: 'K', ru: 'л', ruS: 'Л', ruC: 'Л', structure: '<div class="key"></div>',
   },
   {
-    eng: 'L', rus: 'Д', structure: '<div class="key"></div>',
+    keyCode: 'KeyL', en: 'l', enS: 'L', enC: 'L', ru: 'д', ruS: 'Д', ruC: 'Д', structure: '<div class="key"></div>',
   },
   {
-    eng: ';', rus: 'Ж', structure: '<div class="key"></div>',
+    keyCode: 'Semicolon', en: ';', enS: ':', enC: ';', ru: 'ж', ruS: 'Ж', ruC: 'Ж', structure: '<div class="key"></div>',
   },
   {
-    eng: '\'', rus: 'Э', structure: '<div class="key"></div>',
+    keyCode: 'Quote', en: '\'', enS: '"', enC: '\'', ru: 'э', ruS: 'Э', ruC: 'Э', structure: '<div class="key"></div>',
   },
   {
-    eng: 'ENTER', rus: 'ENTER', structure: '<div class="key key-fun enter"></div>',
+    keyCode: 'Enter', en: 'Enter', enS: 'Enter', enC: 'Enter', ru: 'Enter', ruS: 'Enter', ruC: 'Enter', structure: '<div class="key key-fun enter"></div>',
   }],
   [{
-    eng: 'Shift', rus: 'Shift', structure: '<div class="key key-fun shift-left"></div>',
+    keyCode: 'ShiftLeft', en: 'Shift', enS: 'Shift', enC: 'Shift', ru: 'Shift', ruS: 'Shift', ruC: 'Shift', structure: '<div class="key key-fun shift-left"></div>',
   },
   {
-    eng: 'Z', rus: 'Я', structure: '<div class="key"></div>',
+    keyCode: 'KeyZ', en: 'z', enS: 'Z', enC: 'Z', ru: 'я', ruS: 'Я', ruC: 'Я', structure: '<div class="key"></div>',
   },
   {
-    eng: 'X', rus: 'Ч', structure: '<div class="key"></div>',
+    keyCode: 'KeyX', en: 'x', enS: 'X', enC: 'X', ru: 'ч', ruS: 'Ч', ruC: 'Ч', structure: '<div class="key"></div>',
   },
   {
-    eng: 'C', rus: 'С', structure: '<div class="key"></div>',
+    keyCode: 'KeyC', en: 'c', enS: 'C', enC: 'C', ru: 'с', ruS: 'С', ruC: 'С', structure: '<div class="key"></div>',
   },
   {
-    eng: 'V', rus: 'М', structure: '<div class="key"></div>',
+    keyCode: 'KeyV', en: 'v', enS: 'V', enC: 'V', ru: 'м', ruS: 'М', ruC: 'М', structure: '<div class="key"></div>',
   },
   {
-    eng: 'B', rus: 'И', structure: '<div class="key"></div>',
+    keyCode: 'KeyB', en: 'b', enS: 'B', enC: 'B', ru: 'и', ruS: 'И', ruC: 'И', structure: '<div class="key"></div>',
   },
   {
-    eng: 'N', rus: 'Т', structure: '<div class="key"></div>',
+    keyCode: 'KeyN', en: 'n', enS: 'N', enC: 'N', ru: 'т', ruS: 'Т', ruC: 'Т', structure: '<div class="key"></div>',
   },
   {
-    eng: 'M', rus: 'Ь', structure: '<div class="key"></div>',
+    keyCode: 'KeyM', en: 'm', enS: 'M', enC: 'M', ru: 'ь', ruS: 'Ь', ruC: 'Ь', structure: '<div class="key"></div>',
   },
   {
-    eng: '.', rus: 'Б', structure: '<div class="key"></div>',
+    keyCode: 'Comma', en: ',', enS: '<', enC: ',', ru: 'б', ruS: 'Б', ruC: 'Б', structure: '<div class="key"></div>',
   },
   {
-    eng: ',', rus: 'Ю', structure: '<div class="key"></div>',
+    keyCode: 'Period', en: '.', enS: '>', enC: '.', ru: 'ю', ruS: 'Ю', ruC: 'Ю', structure: '<div class="key"></div>',
   },
   {
-    eng: '/', rus: '.', structure: '<div class="key"></div>',
+    keyCode: 'Slash', en: '/', enS: '?', enC: '/', ru: '.', ruS: ',', ruC: '.', structure: '<div class="key"></div>',
   },
   {
-    eng: '&#9650', rus: '&#9650', structure: '<div class="key"></div>',
+    keyCode: 'ArrowUp', en: '&#9650', enS: '&#9650', enC: '&#9650', ru: '&#9650', ruS: '&#9650', ruC: '&#9650', structure: '<div class="key key-fun"></div>',
   },
   {
-    eng: 'Shift', rus: 'Shift', structure: '<div class="key key-fun shift-right"></div>',
+    keyCode: 'ShiftRight', en: 'Shift', enS: 'Shift', enC: 'Shift', ru: 'Shift', ruS: 'Shift', ruC: 'Shift', structure: '<div class="key key-fun shift-right"></div>',
   }],
   [{
-    eng: 'Ctrl', rus: 'Ctrl', structure: '<div class="key key-fun ctrl"></div>',
+    keyCode: 'ControlLeft', en: 'Ctrl', enS: 'Ctrl', enC: 'Ctrl', ru: 'Ctrl', ruS: 'Ctrl', ruC: 'Ctrl', structure: '<div class="key key-fun ctrl"></div>',
   },
   {
-    eng: 'Win', rus: 'Win', structure: '<div class="key key-fun"></div>',
+    keyCode: 'MetaLeft', en: 'Win', enS: 'Win', enC: 'Win', ru: 'Win', ruS: 'Win', ruC: 'Win', structure: '<div class="key key-fun"></div>',
   },
   {
-    eng: 'Alt', rus: 'Alt', structure: '<div class="key key-fun"></div>',
+    keyCode: 'AltLeft', en: 'Alt', enS: 'Alt', enC: 'Alt', ru: 'Alt', ruS: 'Alt', ruC: 'Alt', structure: '<div class="key key-fun"></div>',
   },
   {
-    eng: '', rus: '', structure: '<div class="key space"></div>',
+    keyCode: 'Space', en: '', enS: '', enC: '', ru: '', ruS: '', ruC: '', structure: '<div class="key space"></div>',
   },
   {
-    eng: 'Alt', rus: 'Alt', structure: '<div class="key key-fun"></div>',
+    keyCode: 'AltRight', en: 'Alt', enS: 'Alt', enC: 'Alt', ru: 'Alt', ruS: 'Alt', ruC: 'Alt', structure: '<div class="key key-fun"></div>',
   },
   {
-    eng: 'Ctrl', rus: 'Ctrl', structure: '<div class="key key-fun ctrl"></div>',
+    keyCode: 'ControlRight', en: 'Ctrl', enS: 'Ctrl', enC: 'Ctrl', ru: 'Ctrl', ruS: 'Ctrl', ruC: 'Ctrl', structure: '<div class="key key-fun ctrl"></div>',
   },
   {
-    eng: '&#9668', rus: '&#9668', structure: '<div class="key key-fun"></div>',
+    keyCode: 'ArrowLeft', en: '&#9668', enS: '&#9668', enC: '&#9668', ru: '&#9668', ruS: '&#9668', ruC: '&#9668', structure: '<div class="key key-fun"></div>',
   },
   {
-    eng: '&#9660', rus: '&#9660', structure: '<div class="key key-fun"></div>',
+    keyCode: 'ArrowDown', en: '&#9660', enS: '&#9660', enC: '&#9660', ru: '&#9660', ruS: '&#9660', ruC: '&#9660', structure: '<div class="key key-fun"></div>',
   },
   {
-    eng: '&#9658', rus: '&#9658', structure: '<div class="key key-fun"></div>',
+    keyCode: 'ArrowRight', en: '&#9658', enS: '&#9658', enC: '&#9658', ru: '&#9658', ruS: '&#9658', ruC: '&#9658', structure: '<div class="key key-fun"></div>',
   }],
 ];
--- src/set-keyboard.js
+++ src/set-keyboard.js
@@ -1,14 +1,53 @@
-// let language = 'eng';
+
+
+let language = 'eng';
+
 const body = document.querySelector('.body');
 
 body.insertAdjacentHTML('afterbegin', '<textarea name="area" id="screen" autofocus></textarea><div class="keyboard"></div');
 const keyboard = document.querySelector('.keyboard');
+const textArea = document.querySelector('#screen');
 
-for (let arr = 0; arr < keys.length; arr += 1) {
-  alert(keyboard.innerHTML);
-  keyboard.insertAdjacentHTML('beforeend', '<div class="row"></div>');
-  const row = keyboard.lastElementChild;
-  for (let el = 0; el < keys[arr].length; el += 1) {
-    row.insertAdjacentHTML('beforeend', keys[arr][el].structure);
+function createKeyboard(keys) {
+  for (let arr = 0; arr < keys.length; arr += 1) {
+    keyboard.insertAdjacentHTML('beforeend', '<div class="row"></div>');
+    const row = keyboard.lastElementChild;
+    for (let el = 0; el < keys[arr].length; el += 1) {
+      row.insertAdjacentHTML('beforeend', keys[arr][el].structure);
+    }
+  }
+}
+
+createKeyboard(keys);
+const virtualKeys = document.querySelectorAll('.key');
+
+function feelKeyboard(keys, language, sh, caps) {
+  let currentKey = 0;
+  for (let arr = 0; arr < keys.length; arr += 1) {
+    for (let el = 0; el < keys[arr].length; el += 1) {
+      virtualKeys[currentKey].innerHTML = keys[arr][el].ru;
+      currentKey += 1;
+    }
+  }
+}
+
+feelKeyboard(keys);
+
+function printKey(event) {
+  const key = findKey(event.code);
+  textArea.value += key.ru;
+  event.preventDefault();
+  // alert(event.code + '  ' + event.key);
+}
+
+addEventListener('keydown', printKey);
+
+function findKey(code) {
+  for (let arr = 0; arr < keys.length; arr += 1) {
+    for (let el = 0; el < keys[arr].length; el += 1) {
+      if (keys[arr][el].keyCode === code) {
+        return keys[arr][el];
+      }
+    }
   }
 }
